c3b872f4c402466f0dec7d503e76d174fbe882cf
thejas-1999 <thejask074@gmail.com>
2024-03-06 14:50:45 +0530

    add a background image in the project by using static file serving method

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set("view engine", "ejs");
 
 //load static assets
 app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //create a homeroute
 
