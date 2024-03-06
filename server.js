ae49d593503e15e15458796f882ca812b0264a9a
thejas-1999 <thejask074@gmail.com>
2024-03-06 12:32:25 +0530

    create a home route and render in to a html page

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//create a homeroute
+
+app.get("/", (req, res) => {
+  res.render();
+});
+
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
 });
